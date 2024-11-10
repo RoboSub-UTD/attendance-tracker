2356ea463d931afa240243eb59b5e67cb64d3a16
Nathan JW <njw230004@utdallas.edu>
2024-11-09 19:36:53 -0600

    make the input field grab focus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
 	let cometCardIDInput: HTMLInputElement;
 	let success: HTMLElement;
+
+	onMount(() => {
+		cometCardIDInput.focus();
+	});
 </script>
 
 <div class="content">
@@ -21,6 +26,7 @@
 			const i1 = cometCardID.indexOf('=');
 			if (i0 === -1 || i1 === -1 || i0 > i1) {
 				cometCardIDInput.value = '';
+				setTimeout(() => { cometCardIDInput.focus(); }, 500);
 				return;
 			}
 
@@ -46,6 +52,7 @@
 				setTimeout(() => {
 					success.innerText = '';
 				}, 3000);
+				cometCardIDInput.focus();
 			}
 		}}
 	>
